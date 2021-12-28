1c4caaa193c8abff36313a045d4c09592756852c
Volodymyr Kovalenko <83116395+hemiechinus@users.noreply.github.com>
2021-12-28 17:33:26 +0200

    finish

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,13 @@
 import user from './components/profile/user.json';
 import data from './components/statistics/data.json';
+import friends from './components/FriendList/friends.json';
+import transactions from './components/transactionHistory/transactions.json';
+
 import Profile from './components/profile/SocialProfile';
 import Statistics from './components/statistics/statistics';
+import FriendList from './components/FriendList/friendList';
+import TransactionHistory from './components/transactionHistory/TransactionHistory';
+
 
 function App() {
     return (
@@ -14,6 +20,8 @@ function App() {
         stats = {user.stats}
         />
         <Statistics title="Upload stats" stats={data}/>
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
       </div>
 
     );
